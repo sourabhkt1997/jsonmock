ba66e45113a213ed29eb8411d5356387fe43f70b
sourabhkt1997 <sourabh.dhanarajan@gmail.com>
2023-08-06 12:56:38 +0530

    nothing

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ let server=jsonServer.create()
 const router=jsonServer.router("db.json");
 
 const port=process.env.port||8080
-server.use(router)
 let cors=require("cors")
 server.use(cors())
-server.listen(port)
+server.use(router)
+server.listen(port,()=>{
+    console.log("Server running on"+port)
+})
